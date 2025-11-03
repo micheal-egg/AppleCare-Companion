48095f9d6f1ee07dd11ae4bba117e47e65e52fe5
Micheal <m.egboh005@gmail.com>
2025-11-02 20:37:35 -0600

    Finished error handling for devices

--- AppleCareCompanion/Services/APIClient.swift
+++ AppleCareCompanion/Services/APIClient.swift
@@ -6,25 +6,52 @@
 //
 
 import Foundation
+import Combine
 
-//Backend Communication
 
+// The class is responsible for talking to the Flask backend.
+// Fetching list og Devices and warranty for a device
+
+@MainActor
 class APIClient: ObservableObject {
     
-    // This property will hold the list of devices we fetched.
-    // @Published means: if this changes, SwiftUI views watching it will update.
+    // This is the array of devices we got from the backend.
+    // @Published means: if this changes, any SwiftUI view using this object will update on screen automatically.
+    
     @Published var devices: [Device] = []
     
-    // This is the base URL of your Flask server.
-    // Make sure Flask is running with `python3 app.py` so this works.
+    // This is where Flask API is running.
+    // Only can be used by this Class
     private let baseURL = "http://127.0.0.1:5000"
-    
-    // Fetch all devices from GET /devices
+
+    //Allowing for asynchronous function
+    //Gets called with "await"
     func fetchDevices() async {
-        // Build the URL: http://127.0.0.1:5000/devices
+        
+        // Converting String to URL object, used by "Url Session"
+        // Building the URL: http://127.0.0.1:5000/devices
+        //Guard, if the link is broken return error message 
         guard let url = URL(string: "\(baseURL)/devices") else {
-            print("❌ Invalid URL for /devices")
+            print("Invalid URL for /devices")
             return
         }
+        
+        do {
+            // Send a GET request and wait for response.
+            // URLSession.shared.data(from:) returns (data, response).
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // Decoding Flask's JSON into Swift structs.
+            // Device must match the JSON style from  Flask /devices route.
+            let decoded = try JSONDecoder().decode([Device].self, from: data)
+            
+            // Store the decoded devices. This triggers UI updates.
+            self.devices = decoded
+            
+        } catch {
+            // If something goes wrong (Flask not running, bad JSON, etc.)
+            print("Error fetching devices:", error)
+        }
     }
+    
 }
